53ca341645e9e7a5840c422f3ce00f171b5fb324
thao9415 <thao.9415@gmail.com>
2019-10-03 11:38:57 +0700

    Triangle

---
+++ src/Shape.java
@@ -0,0 +1,45 @@
+
+    public class Shape {
+        private String color = "green";
+        private boolean filled = true;
+
+        public Shape() {
+        }
+
+
+        public Shape(String color, boolean filled) {
+            this.color = color;
+            this.filled = filled;
+        }
+
+
+        public String getColor() {
+            return this.color;
+        }
+
+        public boolean getFilled() {
+            return this.filled;
+        }
+
+        public void setColor(String color) {
+            this.color = color;
+        }
+
+        public void setFilled(boolean filled) {
+            this.filled = filled;
+        }
+
+        public boolean isFilled() {
+            return filled;
+        }
+
+        @Override
+        public String toString() {
+            return "A Shape with color of "
+                    + getColor()
+                    + " and "
+                    + (isFilled() ? "filled" : "not filled");
+        }
+    }
+
+
---
+++ src/TestTriangle.java
@@ -0,0 +1,8 @@
+public class TestTriangle {
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle();
+        System.out.println(triangle);
+        Triangle triangle1 = new Triangle(5,3,2,"blue",true);
+        System.out.println(triangle1);
+    }
+}
